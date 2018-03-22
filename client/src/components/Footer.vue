1f650c63dcaae5a23b6d45f1c9dee2cd3f444ca4
WJ941 <wangshaosen666@gmail.com>
2018-03-22 14:22:06 +0800

    change footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container class="pa-3 text-sm-left text-xs-center mt-3" >
-    <p>This "fcc-voting" app is built by @arthow4n of freecodecamp,</p>
+    <p>This "fcc-voting" app is built by @wj941 of freecodecamp,</p>
     <p>following the instructions of "Basejump: Build a Voting App | Free Code Camp".</p>
-    <p>Github repository: arthow4n/fcc-voting</p>
+    <p>Github repository:<a href="https://github.com/WJ941/A-Voting-App">wj941/fcc-voting</a></p>
   </v-container>
 </template>
 
